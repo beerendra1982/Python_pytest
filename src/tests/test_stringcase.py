cc524bb83cb369a13d2484e9b40b7f471f1cd168
beerendra1982 <Beerendra1982@gmail.com>
2023-12-03 00:10:28 +0000

    Maing test

--- src/tests/test_stringcase.py
+++ src/tests/test_stringcase.py
@@ -6,4 +6,5 @@ from main.stringcase import to_camel_case
 def test_camelcase():
     expected_result = "BeeruIAmHere"
     result = to_camel_case("Beeru i am here ")
+
     assert expected_result == result
