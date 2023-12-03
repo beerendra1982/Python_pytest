41202a3d06671d8f9fc884e4f3f3f0f5a0d27337
beerendra1982 <Beerendra1982@gmail.com>
2023-12-03 21:58:48 +0000

    Making changes for uni testing

--- src/tests/test_stringcase.py
+++ src/tests/test_stringcase.py
@@ -1,10 +1,11 @@
 import pytest
+import unittest
 
 from main.stringcase import to_camel_case
 
+class TestStringMethods(unittest.TestCase):
 
-def test_camelcase():
-    expected_result = "BeeruIAmHere"
-    result = to_camel_case("Beeru i am here ")
-
-    assert expected_result == result
+    def test_camelcase(self):
+        expected_result = "BeeruIAmHere"
+        result = to_camel_case("Beeru i am here ")
+        self.assertEqual( expected_result , result)
