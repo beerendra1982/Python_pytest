379dba7e3fed85fea17e569f1f3d05234f4bde97
beerendra1982 <Beerendra1982@gmail.com>
2023-12-20 20:27:21 +0000

    Making changes for unit testing

--- src/main/listfunction.py
+++ src/main/listfunction.py
@@ -4,3 +4,18 @@ def minNumber(number):
         if i<min:
             min=i
     return min
+
+def maxNumber(number):
+    min=number[0]
+    for i in number:
+        if i>min:
+            min=i
+    return min
+
+
+def evenNumberList(numbers):
+    even_numbers = [num for num in numbers if num % 2 == 0]
+    print(even_numbers)
+    return even_numbers
+
+
--- src/tests/test_listfunction.py
+++ src/tests/test_listfunction.py
@@ -3,9 +3,20 @@ import unittest
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_minNumber(self):
         from main.listfunction import minNumber
         result=minNumber([1,5,3,0])
         expectedResult=0
         self.assertEqual(result, expectedResult)
 
+    def test_maxNumber(self):
+        from main.listfunction import maxNumber
+        result=maxNumber([1,5,3,0])
+        expectedResult=5
+        self.assertEqual(result, expectedResult)
+
+    def test_evenNumberList(self):
+        from main.listfunction import evenNumberList
+        result =evenNumberList([1,2,3,4,5,6])
+        expectedResult=[2,4,6]
+        self.assertEqual(result, expectedResult)
