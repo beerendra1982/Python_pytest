cf2a317e0f87277e5482dc551bb382aa7bae3ccc
beerendra1982 <Beerendra1982@gmail.com>
2025-05-02 11:27:19 +0100

    Making changes for pytest

--- src/test_game_1.py
+++ src/test_game_1.py
@@ -4,7 +4,13 @@ import gamequiz
 
 def test_quiz2():
     expected_Retuls="15 can be divided by 3 and 5"
-    actual_Result= gamequiz.validateion(151)
+    actual_Result= gamequiz.validateion(15)
     # print(actual_Result)
+    assert expected_Retuls==actual_Result
+
+
+def test_quiz():
+    expected_Retuls="3 can be divided by 3"
+    actual_Result= gamequiz.validateion(3)
     assert expected_Retuls==actual_Result
 
